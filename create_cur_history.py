5bde1e5ebda401d953d2e9e3ba07baf72095253c
reffy <reffymin@gmail.com>
2022-12-21 19:13:03 +0500

    main.py split in two files

--- create_cur_history.py
+++ create_cur_history.py
@@ -116,60 +116,8 @@ def create_cur_history(input_file, output_file):
     print('CSV file created')
 
 
-def convert_cur(salary, publ_date, cur, cur_history):
-    """
-    Конвертирует сумму в рубли, по курсу на момент публикации
-    :param salary: сумма
-    :param publ_date: дата публикации
-    :param cur: валюта
-    :param cur_history: (dataframe) история изменения курсов валют
-    :return: округлённая до 1 знака после запятой сумма
-    """
-    if cur == 'RUR':
-        return salary
-    elif cur not in cur_history.columns:
-        return 0
-    return round(salary * cur_history[cur][publ_date[:7]], 1)
-
-
-def convert_salary(input_file, cur_history_file, output_file):
-    """
-    Изменяет dataframe, создавая новый столбец salary в котором хранится зарплата перевённая в рубли, данные получены
-    из столбцов salary_from, salary_to и salary_currency
-    :param input_file: имя входного файла
-    :param cur_history_file: имя файла хранящего историю изменения курса валют
-    :param output_file: имя выходного файла, в него будут загруженны первые 100 строк итогового dataframe
-    """
-    cur_history = pd.read_csv(cur_history_file, index_col=0)
-    vacancies = pd.read_csv(input_file)
-    print('Files read')
-
-    vacancies = vacancies[vacancies['salary_currency'].notna()]
-    print('NaN rows deleted')
-    vacancies['salary'] = 0
-
-    mask1 = (vacancies['salary_from'].isnull() & ~vacancies['salary_to'].isnull())
-    vacancies['salary'][mask1] = vacancies['salary_to']
-    mask2 = (~vacancies['salary_from'].isnull() & vacancies['salary_to'].isnull())
-    vacancies['salary'][mask2] = vacancies['salary_from']
-    print('Salary row created')
-
-    vacancies['salary'] = vacancies.apply(
-        lambda x: convert_cur(x.salary, x.published_at, x.salary_currency, cur_history), axis=1)
-    print('Salarys converted')
-
-    vacancies = vacancies[vacancies.salary != 0]
-    vacancies = vacancies.drop(columns=['salary_to', 'salary_from', 'salary_currency'])
-    vacancies = vacancies[['name', 'salary', 'area_name', 'published_at']]
-    print('Dataframe ready')
-
-    vacancies.head(100).to_csv(output_file, index=False)
-    print('CSV file created')
-
-
 def main():
-    # create_cur_history('vacancies_dif_currencies.csv', 'currensies_history.csv')
-    convert_salary('csv/vacancies_dif_currencies.csv', 'csv/currensies_history.csv', 'output.csv')
+    create_cur_history('vacancies_dif_currencies.csv', 'currensies_history.csv')
 
 
 if __name__ == "__main__":
